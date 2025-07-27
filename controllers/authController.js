88711aa34cbe9473cc8861cc4ae5215757d4b46a
kristofkruller <kristof.kruller@gmail.com>
2025-07-27 21:33:13 +0200

    registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return res.json({ token, user: payload });
+    return res.json({ token });
 
   } catch (error) {
     console.error('Login error:', error);
@@ -31,8 +31,23 @@ const login = async (req, res) => {
 }
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
+  try {
+    let qry = await pool.query('select * from afonyapp.users where email = $1', [email]);
+
+    if (qry.rowCount > 0) return res.status(401).json({ message: 'Invalid registration, email already exists' });
+
+    const salt = await bcrypt.genSalt(10);
+    const safePassword = await bcrypt.hash(password, salt);
+
+    qry = await pool.query('insert into afonyapp.users (email, password) values ($1,$2)', [email, safePassword]);
+
+    return res.status(201).json({ status: 'Registration successful!' });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ message: 'Server error' })
+  }
 }
 
 module.exports = {
