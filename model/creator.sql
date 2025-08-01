41e35e20b65e0e78602295cc6397dc39a36a4e02
kristofkruller <kristof.kruller@gmail.com>
2025-08-01 12:57:41 +0200

    crea sql fix

--- model/creator.sql
+++ model/creator.sql
@@ -6,6 +6,65 @@ CREATE SCHEMA IF NOT EXISTS afonyapp;
 -- CREATE UNIQUE INDEX users_pkey ON afonyapp.users USING BTREE (id);
 -- CREATE UNIQUE INDEX users_email_key ON afonyapp.users USING BTREE (email);
 
+CREATE TABLE IF NOT EXISTS afonyapp.users(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  "password" TEXT NOT NULL,
+  cdate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "type" VARCHAR(10) NOT NULL DEFAULT 'user',
+  validated BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS afonyapp.amount_options(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  kg INTEGER NOT NULL,
+  cost NUMERIC(10,2) NOT NULL,
+  isvalid BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS afonyapp.delivery_options(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  city VARCHAR(255) NOT NULL,
+  cost NUMERIC(10,2) NOT NULL,
+  isvalid BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS afonyapp.crop(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  available_start DATE NOT NULL,
+  available_end DATE NOT NULL,
+  available_note TEXT NOT NULL,
+  "location" VARCHAR(255) NOT NULL,
+  amount INTEGER NOT NULL
+);
+
+CREATE TYPE afonyapp.delivery_method AS ENUM (
+  'Személyes átvétel',
+  'Házhozszállítás'
+);
+
+CREATE TYPE afonyapp.order_status AS ENUM (
+  'Beérkezett',
+  'Értesített',
+  'Megerősített',
+  'Teljesített',
+  'Lemondott'
+);
+
+CREATE TABLE IF NOT EXISTS afonyapp.orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  cropid INTEGER NOT NULL REFERENCES afonyapp.crop(id),
+  deliverytype afonyapp.delivery_method NOT NULL,
+  deliverycity INTEGER NOT NULL REFERENCES afonyapp.delivery_options(id),
+  deliveryaddress VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL REFERENCES afonyapp.users(email),
+  "name" VARCHAR(255) NOT NULL,
+  telephone VARCHAR(20) NOT NULL,
+  amountid INTEGER NOT NULL REFERENCES afonyapp.amount_options(id),
+  "status" afonyapp.order_status NOT NULL,
+  cdate TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
 -- 🧪 Tesztfelhasználók beszúrása
 -- INSERT INTO afonyapp.users (email, password, type, validated)
 -- VALUES 
@@ -13,61 +72,10 @@ CREATE SCHEMA IF NOT EXISTS afonyapp;
 --   ('testuser2@example.com', '$2b$10$xtPPPaEnRUsJSg.3YLUsoOpP7fObZ2WozNOMgYnvJ90T0xduZFtKu', 'user', true),
 --   ('admin@example.com',     '$2b$10$2shP2vwUJqulBIMLVgdRyuzKPktx7fXHOV4mxUFknmN8qxHTPL9ve', 'admin', true);
 
-CREATE TABLE IF NOT EXISTS users(
-  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "email" VARCHAR(255) NOT NULL UNIQUE,
-  "password" TEXT NOT NULL,
-  "cdate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  "type" VARCHAR(10) NOT NULL DEFAULT 'user',
-  "validated" BOOLEAN NOT NULL DEFAULT false
-);
-
-CREATE TABLE IF NOT EXISTS amount_options(
-  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "kg" INTEGER NOT NULL,
-  "cost" NUMERIC(10,2) NOT NULL,
-  "isvalid" BOOLEAN NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS delivery_options(
-  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "city" VARCHAR(255) NOT NULL,
-  "cost" NUMERIC(10,2) NOT NULL,
-  "isvalid" BOOLEAN NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS crop(
-  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "availableStart" DATE NOT NULL,
-  "availableEnd" DATE NOT NULL,
-  "availableNote" TEXT NOT NULL,
-  "location" VARCHAR(255) NOT NULL,
-  "amount" INTEGER NOT NULL
-);
-
-CREATE TYPE delivery_method AS ENUM (
-  "Személyes átvétel",
-  "Házhozszállítás"
-);
-
-CREATE TYPE order_status AS ENUM (
-  "Beérkezett",
-  "Értesített",
-  "Megerősített",
-  "Teljesített",
-  "Lemondott"
-);
-
-CREATE TABLE IF NOT EXISTS orders(
-  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "cropid" INTEGER NOT NULL REFERENCES crop('id'),
-  "deliverytype" delivery_method NOT NULL,
-  "deliverycity" INTEGER NOT NULL REFERENCES delivery_options('id'),
-  "deliveryaddress" VARCHAR(255) NOT NULL,
-  "email" VARCHAR(255) NOT NULL REFERENCES users('email'),
-  "name" VARCHAR(255) NOT NULL,
-  "telephone" VARCHAR(20) NOT NULL,
-  "amountid" INTEGER NOT NULL REFERENCES amount_options('id'),
-  "status" order_status NOT NULL,
-  "cdate" DATE NOT NULL
-);
+-- 🧪 Minden típus létrejött-e
+-- SELECT n.nspname AS schema, t.typname AS enum_type, e.enumlabel AS value
+-- FROM pg_type t
+-- JOIN pg_enum e ON t.oid = e.enumtypid
+-- JOIN pg_namespace n ON n.oid = t.typnamespace
+-- WHERE n.nspname = 'afonyapp'
+-- ORDER BY enum_type, e.enumsortorder;
